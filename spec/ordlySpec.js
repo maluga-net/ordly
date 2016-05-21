7e1321d4839d1307f09a968ae3db4a46d239a076
maluga_net <c9.io@mi.maluga.net>
2016-05-21 11:10:11 +0000

    * modified configuration for tests

---
+++ spec/ordlySpec.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+var ordly = require('../src/ordly');
+
+describe('ordly', function() {
+    
+    it('should', function() {
+        expect(true).to.be.true;
+    });
+    
+});
