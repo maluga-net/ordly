cc741d556321413db5121ddf7b85a6d6a2dfea7a
maluga_net <c9.io@mi.maluga.net>
2016-06-04 07:01:00 +0000

    + added code and spec for basic scenarios (not finished)

--- spec/ordlySpec.js
+++ spec/ordlySpec.js
@@ -3,8 +3,88 @@ var ordly = require('../src/ordly');
 
 describe('ordly', function() {
     
-    it('should', function() {
-        expect(true).to.be.true;
+    it('should be empty by default', function() {
+        
+        var o = new ordly.Ordly();
+        expect(o.get()).to.be.empty;
     });
     
+    it('should be empty if initilized with empty array', function() {
+        
+        var o = new ordly.Ordly([]);
+        expect(o.get()).to.be.empty;
+        
+    });
+    
+    it('should not be empty if initilized with non empty array', function() {
+        
+        var o = new ordly.Ordly(['foo']);
+        expect(o.get()).to.have.length(1);
+        
+        o = new ordly.Ordly(['foo', 'bar', 'baz']);
+        expect(o.get()).to.have.length(3);
+    });
+
+    it('should have one item if one item added', function() {
+        
+        var o = new ordly.Ordly();
+        expect(o.add('bar')).to.be.empty;
+        expect(o.get()).to.have.length(1);
+    });
+
+    it('should have one item if two items added, but not choosed which is more important', function() {
+        
+        var o = new ordly.Ordly();
+        o.add('bar');
+        o.add('foo');
+        
+        expect(o.get()).to.have.length(1);
+        
+    });
+
+    it('should have two items in correct order if two items added and choosed which is more important', function() {
+        
+        var o = new ordly.Ordly();
+        var item1 ='bar';
+        var item2 = 'foo';
+        
+        var chooseFrom = o.add(item1);
+        
+        expect(chooseFrom).to.be.empty;
+        
+        chooseFrom = o.add(item2);
+        
+        expect(chooseFrom).to.have.length(2);
+        
+        o.choose(chooseFrom[1]);
+        
+        expect(o.get()).to.have.length(2);
+        expect(o.get()).to.deep.equal([item2, item1]);
+    });
+    
+    it('should have two items if one item added to existing two and choosing which is more important not finished', function() {
+        
+        var o = new ordly.Ordly(['foo', 'bar']);
+
+        var chooseFrom = o.add('baz');
+        expect(chooseFrom).to.have.length(2);
+        
+        o.choose(chooseFrom[1]);
+        
+        expect(o.get()).to.have.length(2);
+        expect(o.get()).to.deep.equal(['foo', 'bar']);
+    });
+
+    it('should have three items in correct order if one item added to existing two and choosing which is more important is finished', function() {
+        
+        var o = new ordly.Ordly(['foo','bar']);
+
+        var chooseFrom = o.add('baz');
+        expect(chooseFrom).to.have.length(2);
+        
+        o.choose(chooseFrom[1]);
+        
+        expect(o.get()).to.have.length(3);
+        expect(o.get()).to.deep.equal(['baz', 'foo', 'bar']);
+    });    
 });
--- src/ordly.js
+++ src/ordly.js
@@ -2,6 +2,11 @@
 
 var Ordly = function(elements) {
     
+    if ((typeof elements) === 'undefined') {
+        
+        elements = [];
+    };
+    
     this.elements = elements;
     
 };
@@ -10,6 +15,28 @@ Ordly.prototype.get = function() {
     
     return this.elements;
 }
+
+Ordly.prototype.add = function(item) {
+    
+    var chooseFrom = [];
+    
+    if (this.elements.length > 0) {
+        
+        chooseFrom = [this.elements[0], item];
+        
+    } else {
+    
+        this.elements.push(item);
+    }
+    
+    return chooseFrom;
+}
+    
+Ordly.prototype.choose = function(item) {
+    
+    this.elements.unshift(item);
+}
+    
     
 module.exports = {
     Ordly: Ordly
